f6e7376ceb3bd49fcc331e6484abae770d9739c0
artemiy <shumk1nj@mail.ru>
2025-01-30 13:56:04 +0300

    main

--- JSpractic/app.js
+++ JSpractic/app.js
@@ -1,5 +1,3 @@
-
-
 function checkedorno(){
 
 let checkbox = document.getElementById('check');
@@ -41,4 +39,59 @@ function funfun(){
     let rng =document.getElementById('range')
     let par = document.getElementById('first')
     par.innerHTML = rng.value
+    let div = getComputedStyle.getElementById('test')
+    div.style.width = rng.value +'px'
+}
+
+
+function funrange(){
+    let rtl = document.getElementById('rtl').value;
+    let rtr = document.getElementById('rtr').value;
+    let rbr = document.getElementById('rbr').value;
+    let rbl = document.getElementById('rbl').value;
+    let ttl = document.getElementById('ttl');
+    let ttr = document.getElementById('ttr');
+    let tbr = document.getElementById('tbr');
+    let tbl = document.getElementById('tbl');
+
+
+    let block = document.getElementById('block');
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
+
+
+    block.style.borderRadius = rtl+'px ' +rtr +'px ' +rbr+'px '+rbl+'px ' 
+}
+
+
+
+document.getElementById('nav').onmouseover = function(event){
+    let target = event.target;
+    if(target.className == 'menu-item'){
+        let s = target.getElementsByClassName('submenu')
+        closeMenu();
+        s[0].style.display = 'block'
+    }
+}
+
+
+document.onmouseover =function(event){
+    let target = event.target
+    console.log(event.target)
+    if(target.className != 'menu-item' && target.className !='submenu'){
+        closeMenu()
+    }
+}
+
+function closeMenu(){
+    let menu = getElementById('nav') ;
+    let subm = getElementsByClassName('submenu');
+
+    for(let i = 0 ; i < subm.length;i++ ){
+        subm[i].style.display = 'none';
+
+    }
+
 }
