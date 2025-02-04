7d691d1d43afc2c108a8aa2225816ba0f4975154
artemiy <shumk1nj@mail.ru>
2025-02-04 14:44:27 +0300

    added chat

--- JSpractic/endProject/src/app.js
+++ JSpractic/endProject/src/app.js
@@ -1,6 +1,6 @@
+console.log("JS is connected")
 
-
-
+let cal = document.getElementById("calcu"); 
 function dis(val) { 
     document.getElementById("result").value += val 
 } 
@@ -16,8 +16,17 @@ function myFunction(event) {
         document.getElementById("result").value += event.key; 
 } 
 
-var cal = document.getElementById("calcu"); 
-cal.onkeyup = function (event) { 
+function solve() { 
+    let x = document.getElementById("result").value 
+    let y = math.evaluate(x) 
+    document.getElementById("result").value = y 
+} 
+
+function clr() { 
+    document.getElementById("result").value = "" 
+} 
+
+cal.onkeyup = function(event) { 
     if (event.keyCode === 13) { 
         console.log("Enter"); 
         let x = document.getElementById("result").value 
@@ -26,14 +35,7 @@ cal.onkeyup = function (event) {
     } 
 } 
 
-// Function that evaluates the digit and return result 
-function solve() { 
-    let x = document.getElementById("result").value 
-    let y = math.evaluate(x) 
-    document.getElementById("result").value = y 
-} 
 
-// Function that clear the display 
-function clr() { 
-    document.getElementById("result").value = "" 
-} 
+
+
+
