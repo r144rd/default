edd3fd563f9a8193381c9e0e96d5d3728d2dd646
artemiy <shumk1nj@mail.ru>
2025-02-06 12:17:09 +0300

    added img and many changes

--- JSpractic/endProject/src/app.js
+++ JSpractic/endProject/src/app.js
@@ -35,7 +35,29 @@ cal.onkeyup = function(event) {
     } 
 } 
 
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 
+
+if (btn) { // проверка существования btn
+    console.log('lol kek ')
+    btn.onclick = function(){
+        modal.style.display = "block";
+    }
+}
+
+if (span) { // проверка существования span
+    span.onclick = function(){
+        modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event){
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
